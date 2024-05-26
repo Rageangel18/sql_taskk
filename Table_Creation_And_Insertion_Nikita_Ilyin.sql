97382f5be99aae94f0a86e56c600f039c42ecd45
Rageangel18 <135642896+Rageangel18@users.noreply.github.com>
2024-05-26 22:51:54 +0300

    Fixed

--- Table_Creation_And_Insertion_Nikita_Ilyin.sql
+++ Table_Creation_And_Insertion_Nikita_Ilyin.sql
@@ -23,3 +23,27 @@ UPDATE employees SET last_name = 'Brown' WHERE email = 'jane.smith@example.com';
 SELECT * FROM pg_stat_statements; -- all the changes at config file have been made. I have added 2 new lines at the end
 --  shared_preload_libraries = 'pg_stat_statements' 
 --  pg_stat_statements.track = all
+
+SELECT
+    query,
+    calls
+FROM
+    pg_stat_statements
+ORDER BY
+    calls DESC
+LIMIT 10;
+
+SELECT
+    query,
+    (total_exec_time / calls) AS avg_time
+FROM
+    pg_stat_statements
+ORDER BY
+    avg_time DESC
+LIMIT 10;
+
+SELECT
+    query,
+    total_exec_time
+FROM
+    pg_stat_statements
